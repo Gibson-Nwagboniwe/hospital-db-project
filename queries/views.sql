55ce86759240a5f3fcb1b61d9c5ced0f62076746
Gibson-Nwagboniwe <nwagboniwe.gibson@yahoo.com>
2025-07-30 16:19:47 +0100

    add files

--- queries/views.sql
+++ queries/views.sql
@@ -1,11 +1,14 @@
+--CREATING VIEWS 
+
+--view to return patient with appointment of the current day
 CREATE VIEW todays_appointments AS
 SELECT
     a.appointment_id,
-    p.first_name, p.last_name AS patient_name
-    d.first_name, d.last_name AS doctor
-    a.appointment_date
+    p.first_name, p.last_name AS patient_name,
+    d.first_name, d.last_name AS doctor,
+    a.appointment_date,
     a.reason
 FROM appointments a
-JOIN patient_id
-JOIN doctors d
-WHERE DATE(d.appointment_date) = CURRENT DATE;
+JOIN patients p ON a.patient_id = p.patient_id
+JOIN doctors d ON a.doctor_id = d.doctor_id
+WHERE DATE(a.appointment_date) = CURRENT_DATE;
